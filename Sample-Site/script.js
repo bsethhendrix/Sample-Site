91b2111df5e22a5e313acbd18bb57e9def065cf7
Seth <bsethhendrix@users.noreply.github.com>
2019-08-09 14:10:05 -0400

    adding code for portfolio

---
+++ Sample-Site/script.js
@@ -0,0 +1,17 @@
+$(".custom-button").hover(
+	function() {
+		$(this).css("backgroundColor", "#D4617E");
+	},
+	function() {
+		$(this).css("background", "none");
+	}
+);
+
+$(".dropdown-item").hover(
+	function() {
+		$(this).css("background", "#4B4B4B");
+	},
+	function() {
+		$(this).css("background", "rgba(105, 105, 105, 0.55)");
+	}
+);
